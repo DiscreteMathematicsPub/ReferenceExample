4a25e51b29f919c57be8f98401690008e17ffde1
Josep Roure Alcobé <roure@tecnocampus.cat>
2017-04-06 23:05:54 +0200

    arranged cout to show parameters inside methods

--- src/ReferenceExample.cpp
+++ src/ReferenceExample.cpp
@@ -16,6 +16,8 @@ void swapDoesNotWork (int a , int b) {
 	tmp = a;
 	a = b;
 	b = tmp;
+	cout << "INSIDE: Does not work -->  ";
+	cout << "a: " << a << ", b: " << b << endl;
 }
 
 void swap (int &a, int &b) {
@@ -24,15 +26,17 @@ void swap (int &a, int &b) {
 	tmp = a;
 	a = b;
 	b = tmp;
+	cout << "INSIDE: Does work -->      ";
+	cout << "a: " << a << ", b: " << b << endl;
 }
 
-void stringSwap(string &a, string &b) {
+void stringSwap(string &x, string &y) {
 	string tmp;
 
-	tmp = a;
-	a = b;
-	b = tmp;
-	cout << tmp << " " << b << endl;
+	tmp = x;
+	x = y;
+	y = tmp;
+	cout << "INSIDE: string x: " << x << ", y: " << y << endl;
 }
 
 int main() {
@@ -43,11 +47,11 @@ int main() {
 
 	swapDoesNotWork(x,y);
 	cout << "Does not work -->  ";
-	cout << "x: " << x << ", y: " << y << endl;
+	cout << "x: " << x << ", y: " << y << endl <<endl;
 
 	swap(x,y);
 	cout << "Does work -->      ";
-	cout << "x: " << x << ", y: " << y << endl;
+	cout << "x: " << x << ", y: " << y << endl <<  endl;
 
 	string a("hola");
 	string b("adeu");
